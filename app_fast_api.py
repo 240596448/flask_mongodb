2ec6a2aa7fc172b3541e543e0035939e2fdacb16
Vladimir Nadulich <g240596448@gmail.com>
2022-10-20 21:31:31 +0300

    ping only

--- app_fast_api.py
+++ app_fast_api.py
@@ -1,7 +1,9 @@
 import uvicorn
 from fastapi import FastAPI
+# from fastapi.staticfiles import StaticFiles
 
 app = FastAPI()
+# app.mount("/files", StaticFiles(directory="/home/nadulich/py/"), name="static")
 
 @app.get("/")
 def read_root():
@@ -11,6 +13,20 @@ def read_root():
 def read_root():
     return {"pong":"ok"}
 
+
+# @app.route('/find', methods=['GET','POST'])
+# def find():
+
+    # collection, query = prepare_param(request, 'find')
+
+    # items = collection.find(query)
+    
+    # res = []
+    # for item in items:
+    #     item['_id'] = str(item['_id'])
+    #     res.append(item)
+
+    # return answer(items=res, count=len(res))
 
 if __name__ == "__main__":
     uvicorn.run(app, host="0.0.0.0", port=5050)
